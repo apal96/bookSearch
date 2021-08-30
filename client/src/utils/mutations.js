15b79ad00d1ced2f07d840bdbb82e5c7a14978b1
apal <alishapal96@gmail.com>
2021-08-29 21:59:13 -0700

    add mutation to login user and add user

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER= gql`
+mutation  login($email: String!, $password: String!){
+    login(email:$email, password:$password){
+      user{
+        email
+      }
+    }
+  }
+`;
+
+
+export const ADD_USER= gql`
+mutation createUser($username: String!,$email: String!, $password: String!) {
+    createUser(username:$username, email:$email,password:$password){
+   
+      user{
+          username
+        email
+      }
+    }
+  }
+`;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,7 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Query {
-    users: [Users]!
+   users: [User]!
     user(userId: ID!): User
     me: User
   }
